db5718cfcb6934ba2309869093b8a9178080e0f1
ariniastari <astariarini@gmail.com>
2014-11-17 18:05:39 +0700

    fix twitter

--- wp-content/themes/durex/single-sidebar.php
+++ wp-content/themes/durex/single-sidebar.php
@@ -1,4 +1,5 @@
             <?php $data = load_hashtag_instagram(4);//dd($data);//dd(load_hashtag_twitter());//dd(load_hashtag_instagram());?>
+            <?php $data_tw = load_hashtag_twitter(); ?>
             <div class='col-md-4 col-sm-5 set-height'>
               <div class='row'>
                 <div class='col-xs-12 reset-padding'>
@@ -13,8 +14,8 @@
                     <h3>Social Stream</h3>
                     <div class='line green'></div>
                     <div class='twitter-feed'>
-                      <p class='italic'><?php echo load_hashtag_twitter()[0]->text; ?></p>
-                      <p class='italic-bold'>@<?php echo load_hashtag_twitter()[0]->user->screen_name; ?></p>
+                      <p class='italic'><?php echo $data_tw[0]->text; ?></p>
+                      <p class='italic-bold'>@<?php echo $data_tw[0]->user->screen_name; ?></p>
                       <div class='sprites badges twitter right'></div>
                     </div>
                   </div>
@@ -30,8 +31,8 @@
                 <div class='col-xs-12 blue-sky'>
                   <div class='container-block'>
                     <div class='twitter-feed'>
-                      <p class='italic'><?php echo load_hashtag_twitter()[1]->text; ?></p>
-                      <p class='italic-bold'>@<?php echo load_hashtag_twitter()[1]->user->screen_name; ?></p>
+                      <p class='italic'><?php echo $data_tw[1]->text; ?></p>
+                      <p class='italic-bold'>@<?php echo $data_tw[1]->user->screen_name; ?></p>
                       <div class='sprites badges twitter second right'></div>
                     </div>
                   </div>
