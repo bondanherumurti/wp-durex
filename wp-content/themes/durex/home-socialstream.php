1dd9d7d640937971da9f324d2b90d2994a7d2b2f
ariniastari <astariarini@gmail.com>
2014-11-25 08:42:32 +0700

    remove video, add more tweet

--- wp-content/themes/durex/home-socialstream.php
+++ wp-content/themes/durex/home-socialstream.php
@@ -1,13 +1,9 @@
-<?php $data_ig = load_hashtag_instagram(4); $data_tw = load_hashtag_twitter(2); ?>
+<?php $data_ig = load_hashtag_instagram(4); $data_tw = load_hashtag_twitter(-1);?>
 
 <div class='col-sm-4'>
   <div class='row'>
     <div class='col-xs-12'>
-      <img class='youtube-video youtube-height' data-video='<?php the_field('home_video_url') ?>' src='<?php the_field('display_image'); ?>'>
-      <div class='sprites badges youtube'></div>
-    </div>
-    <div class='col-xs-12'>
-      <div class='white container-block'>
+      <div class='white container-block youtube-height'>
         <h3>Social Stream</h3>
         <div class='line green'></div>
         <div class='twitter-feed'>
@@ -41,6 +37,15 @@
     <div class='col-xs-6 left'>
       <img src='<?php echo $data_ig[3]['images']['standard_resolution']['url'];?>'>
       <div class='sprites badges instagram'></div>
+    </div>
+    <div class='col-xs-12'>
+      <div class='white container-block'>
+        <div class='twitter-feed'>
+          <p class='italic'><?php echo $data_tw[2]->text; ?></p>
+          <p class='italic-bold'>@<?php echo $data_tw[2]->user->screen_name; ?></p>
+          <div class='sprites badges twitter second'></div>
+        </div>
+      </div>
     </div>
   </div>
 </div>
